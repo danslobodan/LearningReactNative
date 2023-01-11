bbbd24bfc678988b8446077a63aa90863a900c63
Slobodan Dan <danastroman@gmail.com>
2023-01-11 13:45:35 +0100

    Completed exercise for ListScreen

--- starter/src/screens/ListScreen.js
+++ starter/src/screens/ListScreen.js
@@ -4,22 +4,20 @@ import { View, Text, StyleSheet, FlatList } from 'react-native'
 const ListScreen = () => {
 
     const friends = [
-        { name: 'Friend #1' },
-        { name: 'Friend #2' },
-        { name: 'Friend #3' },
-        { name: 'Friend #4' },
-        { name: 'Friend #5' },
-        { name: 'Friend #6' },
-        { name: 'Friend #7' }
+        { name: 'Friend #1', age: 20 },
+        { name: 'Friend #2', age: 25 },
+        { name: 'Friend #3', age: 17 },
+        { name: 'Friend #4', age: 64 },
+        { name: 'Friend #5', age: 4 },
+        { name: 'Friend #6', age: 45 },
+        { name: 'Friend #7', age: 39 }
     ]
 
     return <FlatList 
-        horizontal
-        showsHorizontalScrollIndicator={false}
         keyExtractor={(friend) => friend.name}
         data={friends}
         renderItem={({ item }) => {
-            return <Text style={styles.textStyle}>{item.name}</Text>
+            return <Text style={styles.textStyle}>{item.name} - Age {item.age}</Text>
         }}>
     </FlatList>
 }
@@ -27,7 +25,7 @@ const ListScreen = () => {
 const styles = StyleSheet.create({
 
     textStyle: {
-        marginVertical: 50
+        marginVertical: 10
     }
 })
 
