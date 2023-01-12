4564c1074d4aae6b2641bd9bb8139c3709db5714
Slobodan Dan <danastroman@gmail.com>
2023-01-12 14:48:56 +0100

    Added color screen.

--- starter/src/screens/HomeScreen.js
+++ starter/src/screens/HomeScreen.js
@@ -22,6 +22,10 @@ const HomeScreen = ({ navigation }) => {
           title="Go to counter demo"
           onPress={() => navigation.navigate('Counter')}
         />
+        <Button 
+          title="Go to color demo"
+          onPress={() => navigation.navigate('Color')}
+        />
     </View>
   )
 };
