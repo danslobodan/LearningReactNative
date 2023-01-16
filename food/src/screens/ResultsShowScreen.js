70f8ebe5201575930672007d765066e4c4da59a4
Slobodan Dan <danastroman@gmail.com>
2023-01-16 18:26:01 +0100

    Added images to results list.

--- food/src/screens/ResultsShowScreen.js
+++ food/src/screens/ResultsShowScreen.js
@@ -1,14 +1,43 @@
-import React from "react";
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Text, Image, StyleSheet, FlatList } from 'react-native';
+import yelp from '../api/yelp';
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({ navigation }) => {
+
+    const [result, setResult] = useState(null);
+    const id = navigation.getParam('id');
+
+    const getResult = async (id) => {
+        const response = await yelp.get(`${id}`);
+        setResult(response.data);
+    }
+
+    useEffect(() => {
+        getResult(id);
+    }, []);
+
+    if (!result)
+        return null;
+
     return (
         <View>
             <Text>Results Show</Text>
+            <FlatList
+                data={result.photos}
+                keyExtractor={(photo) => photo}
+                renderItem={({ item }) => {
+                    return <Image style={styles.imageStyle} source={{ uri: item }} />
+                }}
+            />
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    imageStyle: {
+        height: 300,
+        width: 300
+    }
+})
 
 export default ResultsShowScreen;
