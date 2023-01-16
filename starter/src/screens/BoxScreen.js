03a4d2929cc054dfe0fadb297ab2f7831c844f2f
Slobodan Dan <danastroman@gmail.com>
2023-01-16 10:39:51 +0100

    Added box screen

---
+++ starter/src/screens/BoxScreen.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+
+const BoxScreen = () => {
+    return (
+        <View style={styles.viewStyle}>
+            <View style={styles.colorBoxOneStyle} />
+            <View style={styles.colorBoxTwoStyle} />
+            <View style={styles.colorBoxThreeStyle} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 2,
+        height: 160,
+        borderColor: 'black',
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    colorBoxOneStyle: {
+        height: 80, 
+        width: 100, 
+        backgroundColor: 'rgb(255, 120, 120)' f                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+    },
+    colorBoxTwoStyle: {
+        alignSelf: "flex-end",
+        height: 80, 
+        width: 100, 
+        backgroundColor: 'rgb(120, 255, 120)' 
+    },
+    colorBoxThreeStyle: {
+        height: 80, 
+        width: 100, 
+        backgroundColor: 'rgb(120, 120, 255)' 
+    }
+})
+
+export default BoxScreen;
