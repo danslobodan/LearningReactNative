1546f3747d9aca4a9a88af86938153d9177d31f3
Slobodan Dan <danastroman@gmail.com>
2023-01-16 17:28:57 +0100

    Showing a result list.

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from 'react-native';
+import ResultsList from "../components/ResultsList";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
 
@@ -7,6 +8,12 @@ const SearchScreen = () => {
 
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
+
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price == price;
+        })
+    }
 
     return (
         <View style={styles.background}>
@@ -16,7 +23,9 @@ const SearchScreen = () => {
                 onTermSubmit={() => searchApi(term)}
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
-            <Text>We have found {results.length} results</Text>
+            <ResultsList results={filterResultsByPrice("$")} title="Cost Effective" />
+            <ResultsList results={filterResultsByPrice("$$")} title="Bit Priciers" />
+            <ResultsList results={filterResultsByPrice("$$$")} title="Big Spender" />
         </View>
     )
 }
