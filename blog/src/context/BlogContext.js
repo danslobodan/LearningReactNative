108f5cfbb4aed130dc2ef8fc4c0645290ef88b09
Slobodan Dan <danastroman@gmail.com>
2023-01-17 12:23:02 +0100

    Added add function for adding blogs to context.

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
 
-    const blogPosts = [
+    const [blogPosts, setBlogPosts] = useState([
         { title: 'Blog Post #1' },
         { title: 'Blog Post #2' }
-    ]
+    ]);
+
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }])
+    }
 
     return (
-        <BlogContext.Provider value={blogPosts}>
+        <BlogContext.Provider value={
+        { 
+            data: blogPosts, 
+            addBlogPost 
+        }}>
             {children}           
         </BlogContext.Provider>
     )
