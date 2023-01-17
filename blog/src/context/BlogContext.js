250514c6d44ebe0101583420112c9c4211953377
Slobodan Dan <danastroman@gmail.com>
2023-01-17 12:32:27 +0100

    Refactored blog context to use a reducer.

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,23 +1,29 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+    switch(action.type) {
+        case 'add_blogpost':
+            return [...state, { title: `Blog Post #${state.length + 1}`}];
+        default:
+            return state;
+    }
+}
+
 export const BlogProvider = ({ children }) => {
 
-    const [blogPosts, setBlogPosts] = useState([
-        { title: 'Blog Post #1' },
-        { title: 'Blog Post #2' }
-    ]);
+    const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
     const addBlogPost = () => {
-        setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }])
+        dispatch({ type: 'add_blogpost' })
     }
 
     return (
         <BlogContext.Provider value={
         { 
-            data: blogPosts, 
-            addBlogPost 
+            data: blogPosts,
+            addBlogPost
         }}>
             {children}           
         </BlogContext.Provider>
