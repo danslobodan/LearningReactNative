f6097c9eb823015a9639a392336a1ebfe236114e
Slobodan Dan <danastroman@gmail.com>
2023-01-19 17:43:12 +0100

    Hashing and salting passwords.

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -21,4 +21,26 @@ router.post('/signup', async (req, res) => {
     }
 })
 
+router.post('/signin', async (req, res) => {
+
+    const { email, password } = req.body;
+
+    if (!email || !password)
+        return res.status(422).send({ error: 'Must provide email and password' });
+
+    const user = await User.findOne({ email });
+
+    if (!user)
+        return res.status(404).send({ error: 'Invalid username or password' });
+
+    try {
+        await user.comparePassworde(password);
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_KEY);
+        res.send({ token });
+    } catch (err) {
+        return res.status(422).send({ error: 'Invalid username or password' });
+    }
+    
+})
+
 module.exports = router;
