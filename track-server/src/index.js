162504333b78b076d292b9211d0f453fed1e98bf
Slobodan Dan <danastroman@gmail.com>
2023-01-19 12:36:00 +0100

    Added auth route.

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,10 +1,17 @@
+require('./models/User');
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+const authRoutes = require('./routes/authRoutes')
 
 dotenv.config();
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
 
 const mongoUri = `mongodb+srv://dbuser:${process.env.MONGOPASSWORD}@cluster0.stmbi.mongodb.net/?retryWrites=true&w=majority`;
 
