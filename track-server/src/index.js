cddcb1f9b43840ea0653705dd442b31f940c83fd
Slobodan Dan <danastroman@gmail.com>
2023-01-19 12:12:36 +0100

    Setup bacis express api.

---
+++ track-server/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+const mongoUri = `mongodb+srv://dbuser:${process.env.MONGOPASSWORD}@cluster0.stmbi.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', true);
+mongoose.connect(mongoUri);
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+})
+
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo', err);
+})
+
+app.get('/', (req, res) => {
+    res.send('Hi there!')
+})
+
+app.listen(3000, () => {
+    console.log('Listening no port 3000')
+})
+
