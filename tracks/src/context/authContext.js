fd68222a5f8b6f193489946b9220a027aa4b5173
Slobodan Dan <danastroman@gmail.com>
2023-01-20 20:15:34 +0100

    Added axios api and implemented sign up.

--- tracks/src/context/authContext.js
+++ tracks/src/context/authContext.js
@@ -1,14 +1,48 @@
 import createDataContext from "./createDataContext";
-
+import trackerApi from '../api/tracker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+ 
 const authReducer = (state, action) => {
     switch (action.type) {
+        case 'add_error':
+            return { ...state, errorMessage: action.payload };
+        case 'signup':
+            return { token: action.payload, errorMessage: '' };
         default:
             return state;
     }
 }
 
+const signUp = (dispatch) => async ({ email, password }) => {
+
+    try {
+        const response = await trackerApi.post('/signup', { email, password });
+        await AsyncStorage.setItem('token', response.data.token);
+        dispatch({ type: 'signup', payload: response.data.token });
+    } catch (error) {
+        dispatch({ type: 'add_error', payload: 'Something went wrong with signup '});
+    }
+}
+
+const signIn = (dispatch) => {
+    return async ({ email, password }) => {
+
+
+    }
+}
+
+const signOut = (dispatch) => {
+    return () => {
+
+
+    }
+}
+
 export const { Provider, Context } = createDataContext(
     authReducer,
-    {},
-    { isSignedIn: false }
+    { signUp, signIn, signOut },
+    { 
+        token: null,
+        errorMessage: '' 
+    }
 )
--- tracks/src/screens/SignUpScreen.js
+++ tracks/src/screens/SignUpScreen.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { StyleSheet, View } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from "../components/Spacer";
+import { Context as AuthContext } from '../context/authContext';
 
 const SignUpScreen = ({ navigation }) => {
 
+    const { state, signUp } = useContext(AuthContext);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -29,8 +31,9 @@ const SignUpScreen = ({ navigation }) => {
                 autoCorrect={false}
                 secureTextEntry
             />
+            {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
             <Spacer>
-                <Button title="Sign Up" onPress={() => navigation.navigate('SignIn')} />
+                <Button title="Sign Up" onPress={() => signUp({ email, password })} />
             </Spacer>
         </View>
     )
@@ -49,6 +52,12 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         marginBottom: 250
+    },
+    errorMessage: {
+        fontSize: 16,
+        color: 'red',
+        marginLeft: 15,
+        marginTop: 15
     }
 })
 
