475ce7df846eba7fab74b9e41aa642268454bfbd
Slobodan Dan <danastroman@gmail.com>
2023-01-23 11:19:25 +0100

    Added swipe animation.

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -1,9 +1,18 @@
 import React, { useRef } from "react";
-import { Animated, PanResponder } from 'react-native';
+import { Dimensions, Animated, PanResponder } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 const Deck = ({ data, renderCard }) => {
 
     const position = useRef(new Animated.ValueXY()).current;
+
+    const resetPosition = () => {
+        Animated.spring(position, {
+            toValue: { x: 0, y: 0 },
+            useNativeDriver: false
+        }).start();
+    }
 
     const panResponder = useRef(PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -11,20 +20,43 @@ const Deck = ({ data, renderCard }) => {
             position.setValue({ x: gesture.dx , y: gesture.dy });
         },
         onPanResponderRelease: () => {
+            resetPosition();
         }
     })).current;
 
+    const getCardStyle = () => {
 
-    const renderCards = () => data.map(item => renderCard(item));
+        const rotationMultiplier = 2;
 
-    return (
-        <Animated.View 
-            style={position.getLayout()}
-            {...panResponder.panHandlers}
-        >
-            {renderCards()}
-        </Animated.View>
-    )
+        const rotate = position.x.interpolate({
+            inputRange: [-SCREEN_WIDTH * rotationMultiplier, 0, SCREEN_WIDTH * rotationMultiplier],
+            outputRange: ['-120deg', '0deg', '120deg']
+        });
+
+        return {
+            ...position.getLayout(),
+            transform: [{ rotate }]
+        };
+    }
+
+
+    const renderCards = () => data.map((item, i) => {
+        if (i === 0) {
+            return (
+                <Animated.View
+                    key={item.id}
+                    style={getCardStyle()}
+                    {...panResponder.panHandlers}
+                >
+                    {renderCard(item)}
+                </Animated.View>
+            )
+        }
+
+        return renderCard(item);
+    });
+
+    return renderCards();
 }
 
 export default Deck;
