102d7b36504cd27bb5508c1b6a529c03d3b5b049
Slobodan Dan <danastroman@gmail.com>
2023-01-23 11:38:48 +0100

    Added swipe left and right animation.

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -2,8 +2,10 @@ import React, { useRef } from "react";
 import { Dimensions, Animated, PanResponder } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
-const Deck = ({ data, renderCard }) => {
+const Deck = ({ data, renderCard, onSwipeLeft, onSwipeRight }) => {
 
     const position = useRef(new Animated.ValueXY()).current;
 
@@ -14,19 +16,46 @@ const Deck = ({ data, renderCard }) => {
         }).start();
     }
 
+    const onSwipeComplete = (direction) => {
+
+        direction === 'right'
+            ? onSwipeRight()
+            : onSwipeLeft();
+    }
+
+    const forceSwipe = (direction) => {
+
+        const x = direction === 'right' 
+            ? SCREEN_WIDTH
+            : -SCREEN_WIDTH;
+
+        Animated.timing(position, {
+            toValue: { x, y: 0 },
+            duration: SWIPE_OUT_DURATION,
+            useNativeDriver: false
+        }).start(() => onSwipeComplete(direction));
+    }
+
     const panResponder = useRef(PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderMove: (event, gesture) => {
             position.setValue({ x: gesture.dx , y: gesture.dy });
         },
-        onPanResponderRelease: () => {
-            resetPosition();
+        onPanResponderRelease: (event, gesture) => {
+            if (gesture.dx > SWIPE_THRESHOLD) {
+                forceSwipe('right');
+            }
+            else if (gesture.dx < -SWIPE_THRESHOLD) {
+                forceSwipe('left');
+            }
+            else
+                resetPosition();
         }
     })).current;
 
     const getCardStyle = () => {
 
-        const rotationMultiplier = 2;
+        const rotationMultiplier = 1.5;
 
         const rotate = position.x.interpolate({
             inputRange: [-SCREEN_WIDTH * rotationMultiplier, 0, SCREEN_WIDTH * rotationMultiplier],
