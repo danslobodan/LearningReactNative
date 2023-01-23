0e70dbbfaeb880c36437e66c41bd98f5b6435ea0
Slobodan Dan <danastroman@gmail.com>
2023-01-23 13:22:34 +0100

    Card bounce back.

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -1,14 +1,30 @@
-import React, { useRef, useState } from "react";
-import { Dimensions, Animated, PanResponder } from 'react-native';
+import React, { useEffect, useRef, useState } from "react";
+import { 
+    StyleSheet, 
+    Dimensions, 
+    Animated, 
+    PanResponder,
+    LayoutAnimation,
+    UIManager
+} from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
-const Deck = ({ data, renderCard, renderNoMoreCards }) => {
+const Deck = ({ data, renderCard, onSwipeLeft, onSwipeRight, renderNoMoreCards }) => {
 
     const [index, setIndex] = useState(0);
     const position = useRef(new Animated.ValueXY()).current;
+
+    useEffect(() => {
+        setIndex(0);
+    }, [data]);
+
+    useEffect(() => {
+        UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+        LayoutAnimation.spring();
+    });
 
     const resetPosition = () => {
         Animated.spring(position, {
@@ -94,7 +110,7 @@ const Deck = ({ data, renderCard, renderNoMoreCards }) => {
                 return (
                     <Animated.View
                         key={item.id}
-                        style={getCardStyle()}
+                        style={[getCardStyle(), styles.card]}
                         {...panResponder.panHandlers}
                     >
                         {renderCard(item)}
@@ -102,9 +118,16 @@ const Deck = ({ data, renderCard, renderNoMoreCards }) => {
                 )
             }
 
-            return renderCard(item);
-        }
-    )};
+            return (
+                <Animated.View 
+                    key={item.id}
+                    style={[styles.card, { top: (i - index) * 10 }]}
+                >
+                    {renderCard(item)}
+                </Animated.View>
+            )
+        }).reverse();
+    };
 
     return renderCards();
 }
@@ -113,5 +136,12 @@ Deck.defaultProps = {
     onSwipeRight: () => {},
     onSwipeLeft: () => {}
 }
+
+const styles = StyleSheet.create({
+    card: {
+        position: 'absolute',
+        width: SCREEN_WIDTH
+    }
+})
 
 export default Deck;
